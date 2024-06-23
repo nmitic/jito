f037bdad201dd1f1cdd43dc15e34de534a7426c8
Nikola Mitic <nikola.mitic.dev@gmail.com>
2024-06-23 16:43:49 +0200

    clean up

--- src/extension.ts
+++ src/extension.ts
@@ -13,20 +13,39 @@ function removeFileExtension(filePath: string): string {
   return newPath;
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "jito" is now active!');
+let GLOBAL_FLAGS = {
+  JITO_IGNORED: false,
+  BROWSER_OPEN: false,
+};
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("jito.jito", async () => {
-    // The code you place here will be executed every time your command is executed
+const updateGitIgnoreFile = async (root: vscode.Uri) => {
+  console.log(GLOBAL_FLAGS);
+  if (GLOBAL_FLAGS.JITO_IGNORED) {
+    return;
+  }
 
-    const tmpl = `
+  const gitignoreTmpl = `
+# JITO
+app/jito/page.tsx
+		`;
+
+  const gitIgnoreFile = root.with({
+    path: posix.join(root.path, ".gitignore"),
+  });
+
+  const gitIgnoreOldContent = await vscode.workspace.fs.readFile(gitIgnoreFile);
+  const gitIgnoreOldText = new TextDecoder().decode(gitIgnoreOldContent);
+  const gitIgnoreNewText = `${gitIgnoreOldText} ${gitignoreTmpl}`;
+
+  const gitIgnoreNewContent = new TextEncoder().encode(gitIgnoreNewText);
+
+  await vscode.workspace.fs.writeFile(gitIgnoreFile, gitIgnoreNewContent);
+
+  GLOBAL_FLAGS.JITO_IGNORED = true;
+};
+
+const generateJitoPage = async (root: vscode.Uri) => {
+  const tmpl = `
 "use client"
 
 importPlaceholder
@@ -38,66 +57,64 @@ const Jito = () => {
 export default Jito;
 			`;
 
-    const gitignoreTmpl = `
-# JITO
-app/jito/page.tsx
-		`;
+  const folder = root.with({ path: posix.join(root.path, "app/jito") });
+  const file = root.with({
+    path: posix.join(root.path, "app/jito/page.tsx"),
+  });
 
+  const componentFilePath = vscode.window.activeTextEditor?.document.uri
+    .path as string;
+  const userTextSelection = vscode.window.activeTextEditor?.selection;
+  const componentName =
+    vscode.window.activeTextEditor?.document.getText(userTextSelection);
+  const componentImportPath = removeFileExtension(
+    relative(folder.path, componentFilePath)
+  );
+
+  await vscode.workspace.fs.createDirectory(folder);
+  await vscode.workspace.fs.writeFile(file, Buffer.from(tmpl));
+
+  // Read the current content of the file
+  const oldContent = await vscode.workspace.fs.readFile(file);
+  const oldText = new TextDecoder().decode(oldContent);
+
+  // Define the new content
+  const newText = oldText
+    .replace("JitoComponentPlaceholder", `<${componentName} />`)
+    .replace(
+      "importPlaceholder",
+      `import { ${componentName} } from "${componentImportPath}"`
+    );
+
+  // Write the new content to the file
+  const newContent = new TextEncoder().encode(newText);
+
+  await vscode.workspace.fs.writeFile(file, newContent);
+};
+
+const openBrowser = async () => {
+  if (GLOBAL_FLAGS.BROWSER_OPEN) {
+    return;
+  }
+
+  vscode.commands.executeCommand("simpleBrowser.show");
+
+  GLOBAL_FLAGS.BROWSER_OPEN = true;
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "jito" is now active!');
+
+  const disposable = vscode.commands.registerCommand("jito.jito", async () => {
     const root = vscode.workspace.workspaceFolders![0]!.uri;
 
-    const folder = root.with({ path: posix.join(root.path, "app/jito") });
-    const file = root.with({
-      path: posix.join(root.path, "app/jito/page.tsx"),
-    });
-    const gitIgnoreFile = root.with({
-      path: posix.join(root.path, ".gitignore"),
-    });
+    generateJitoPage(root);
+    updateGitIgnoreFile(root);
 
-    console.log(gitIgnoreFile.path);
-
-    const componentFilePath = vscode.window.activeTextEditor?.document.uri
-      .path as string;
-    const userTextSelection = vscode.window.activeTextEditor?.selection;
-    const componentName =
-      vscode.window.activeTextEditor?.document.getText(userTextSelection);
-    const componentImportPath = removeFileExtension(
-      relative(folder.path, componentFilePath)
-    );
-
-    await vscode.workspace.fs.createDirectory(folder);
-    await vscode.workspace.fs.writeFile(file, Buffer.from(tmpl));
-
-    // Read the current content of the file
-    const oldContent = await vscode.workspace.fs.readFile(file);
-    const oldText = new TextDecoder().decode(oldContent);
-    const gitIgnoreOldContent = await vscode.workspace.fs.readFile(
-      gitIgnoreFile
-    );
-    const gitIgnoreOldText = new TextDecoder().decode(gitIgnoreOldContent);
-
-    // Define the new content
-    const newText = oldText
-      .replace("JitoComponentPlaceholder", `<${componentName} />`)
-      .replace(
-        "importPlaceholder",
-        `import { ${componentName} } from "${componentImportPath}"`
-      );
-
-    const gitIgnoreNewText = `${gitIgnoreOldText} ${gitignoreTmpl}`;
-
-    // Write the new content to the file
-    const newContent = new TextEncoder().encode(newText);
-    const gitIgnoreNewContent = new TextEncoder().encode(gitIgnoreNewText);
-
-    await vscode.workspace.fs.writeFile(file, newContent);
-    await vscode.workspace.fs.writeFile(gitIgnoreFile, gitIgnoreNewContent);
-
-    vscode.commands.executeCommand("simpleBrowser.show");
-    console.log(process.env.VSCODE_BROWSER_TEST);
+    openBrowser();
   });
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
